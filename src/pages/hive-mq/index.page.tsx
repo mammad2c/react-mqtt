d6e71973723d61b128b4296f50b09e5ac637b18c
Mohammad Toosi <m_dd@rogers.com>
2025-07-21 21:28:50 +0100

    chore: better layout and more readable texts

--- src/pages/hive-mq/index.page.tsx
+++ src/pages/hive-mq/index.page.tsx
@@ -1,9 +1,18 @@
+import { Center, Container, Text } from "@chakra-ui/react";
 import ConnectionForm from "./ui/connection-form";
+import Link from "next/link";
 
 export default function HiveMQ() {
   return (
-    <div>
-      <ConnectionForm />
-    </div>
+    <Container py={8}>
+      <Center flexDirection="column">
+        <ConnectionForm />
+        <Text as={"div"} pt={4}>
+          <Link className="mt-4" href="/">
+            Back to Home
+          </Link>
+        </Text>
+      </Center>
+    </Container>
   );
 }
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Center, Container, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Button colorPalette="yellow" asChild>
-        <Link href="/hive-mq">HiveMQ</Link>
-      </Button>
-    </div>
+    <Container>
+      <Center p={8} flexDirection="column">
+        <Text>Welcome to the MQTT Dashboard</Text>
+        <Text>Explore the various MQTT brokers available.</Text>
+
+        <Text fontWeight="bold" my={8}>
+          Warning: This project is only a demo and not intended for production
+          use.
+        </Text>
+
+        <Text mb={4}>Available brokers:</Text>
+        <div>
+          <Button colorPalette="yellow" asChild>
+            <Link href="/hive-mq">HiveMQ</Link>
+          </Button>
+        </div>
+      </Center>
+    </Container>
   );
 }
